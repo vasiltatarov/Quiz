a467e1227413c8677a697b4fa3a3f8ad45960a96
vasiltatarov <vasiltatarov3@gmail.com>
2021-06-13 13:42:43 +0300

    add statistics controller with view

---
+++ src/Web/Quiz.Web/Controllers/StatisticsController.cs
@@ -0,0 +1,16 @@
+﻿namespace Quiz.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class StatisticsController : Controller
+    {
+        public StatisticsController()
+        {
+        }
+
+        public IActionResult UserStatistics()
+        {
+            return this.View();
+        }
+    }
+}
