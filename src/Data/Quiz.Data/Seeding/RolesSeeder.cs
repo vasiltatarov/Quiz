8da2b3ba19cee916a77a13c69d62a3f977f67a34
vasiltatarov <vasiltatarov3@gmail.com>
2021-06-14 18:06:00 +0300

    seed administrator role

--- src/Data/Quiz.Data/Seeding/RolesSeeder.cs
+++ src/Data/Quiz.Data/Seeding/RolesSeeder.cs
@@ -11,11 +11,33 @@
 
     internal class RolesSeeder : ISeeder
     {
+        private const string AdminName = "Vasil@gmail.com";
+
         public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
             await SeedRoleAsync(roleManager, GlobalConstants.AdministratorRoleName);
+            await SeedUserToRoleAsync(userManager, GlobalConstants.AdministratorRoleName);
+        }
+
+        private static async Task SeedUserToRoleAsync(UserManager<ApplicationUser> userManager, string roleName)
+        {
+            var user = await userManager.FindByEmailAsync(AdminName);
+
+            if (user == null)
+            {
+                return;
+            }
+
+            var isInRole = await userManager.IsInRoleAsync(user, roleName);
+            if (isInRole)
+            {
+                return;
+            }
+
+            await userManager.AddToRoleAsync(user, roleName);
         }
 
         private static async Task SeedRoleAsync(RoleManager<ApplicationRole> roleManager, string roleName)
